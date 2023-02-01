a6c23b8f3cf19426ba1074025812a751d6920893
Xavi1594 <xavi9415@hotmail.com>
2023-02-01 19:21:39 +0100

    first commit

---
+++ src/main/java/coches/Automovil.java
@@ -0,0 +1,31 @@
+package coches;
+
+public class Automovil {
+
+    private String marca;
+    private short modelo;
+    private short motor;
+    private enum tipoCombustible {
+GASOLINA, DIESEL, BIODIESEL, BIOETANOL, GASNATURAL
+    };
+    private tipoCombustible tipo;
+    private enum tipoAutomovil {
+UTILITARIO, SUBCOMPACTO, COMPACTO, FAMILIAR, EJECUTIVO, SUV};
+private tipoAutomovil tipo;
+private byte numeroPuertas;
+private byte cantidadAsientos;
+private short cantidadVehiculos;
+private short velocidadMaxima;
+private enum color {
+ BLANCO, VERDE, AMARILLO, AZUL, VIOLETA, NEGRO, ROJO, NARANJA}
+ private color color;
+private short velocidadActual;
+ 
+
+
+ 
+
+
+
+
+}
